9c8cf3dcca54c36e1539c8bede1a2cee8d54e6e7
SteTheDiver <stefano.cattaneo86@gmail.com>
2022-07-06 18:34:55 +0200

    Pages structure

---
+++ src/pages/Landing.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import main from "../assets/images/main.svg";
+import Wrapper from "../assets/wrappers/LandingPage";
+import { Logo } from "../components";
+
+const Landing = () => {
+  return (
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            Job <span>Tracking</span> App
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium
+            voluptatibus et reprehenderit nisi, magnam non consequatur?
+            Voluptates totam accusantium, a eaque repellendus eveniet, ut
+            aspernatur consectetur quia neque iusto nam.
+          </p>
+          <button className="btn">Login/Register</button>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </Wrapper>
+  );
+};
+
+export default Landing;
