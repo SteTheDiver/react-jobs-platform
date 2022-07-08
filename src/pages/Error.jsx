64882ec2b28fa3ca14c3d2005f0860a64a410028
SteTheDiver <stefano.cattaneo86@gmail.com>
2022-07-08 19:37:03 +0200

    Created error page with styling

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,8 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
+import pageNotFound from "../assets/images/pageNotFound.svg";
+import Wrapper from "../assets/wrappers/ErrorPage";
 
 const Error = () => {
-  return <div>Error</div>;
+  return (
+    <Wrapper className="full-page">
+      <div>
+        <img src={pageNotFound} alt="not found" />
+        <h3>Page not Found</h3>
+        <p>The page you are looking for is not here</p>
+        <Link to="/">Back home</Link>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Error;
