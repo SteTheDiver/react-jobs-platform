ab439588609a96be1e9cf30bda6ce6453dd66857
SteTheDiver <stefano.cattaneo86@gmail.com>
2022-07-10 21:39:10 +0200

    Updated routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import { Landing, Error, Register, Dashboard } from "./pages";
+import { Landing, Error, Register } from "./pages";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import {
+  Profile,
+  AllJobs,
+  AllJobs,
+  Statics,
+  SharedLayout,
+} from "./pages/Dashboard";
+import NewJob from "./pages/Dashboard/NewJob";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<Statics />} />
+          <Route path="all-jobs" element={<AllJobs />} />
+          <Route path="new-job" element={<NewJob />} />
+          <Route path="profile" element={<Profile />} />
+        </Route>
         <Route path="landing" element={<Landing />} />
         <Route path="register" element={<Register />} />
         <Route path="*" element={<Error />} />
