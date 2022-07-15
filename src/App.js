8ecf73726220daf150944560bd72a5093dee9c43
SteTheDiver <stefano.cattaneo86@gmail.com>
2022-07-15 14:09:24 +0200

    Added protected route

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import { Landing, Error, Register } from "./pages";
+import { Landing, Error, Register, ProtectedRoute } from "./pages";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import {
-  Profile,
-  AllJobs,
-  AllJobs,
-  Statics,
-  SharedLayout,
-} from "./pages/Dashboard";
+import { Profile, AllJobs, Statistics, SharedLayout } from "./pages/Dashboard";
 import NewJob from "./pages/Dashboard/NewJob";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<SharedLayout />}>
-          <Route index element={<Statics />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <SharedLayout />
+            </ProtectedRoute>
+          }
+        >
+          <Route index element={<Statistics />} />
           <Route path="all-jobs" element={<AllJobs />} />
           <Route path="new-job" element={<NewJob />} />
           <Route path="profile" element={<Profile />} />
