c12be5d8ac344976ef14dfe8fff3374fe400d646
SteTheDiver <stefano.cattaneo86@gmail.com>
2022-07-15 14:40:13 +0200

    Completed sidebars with logic

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { useSelector } from "react-redux";
+
+import Wrapper from "../../assets/wrappers/Sidebar";
+import NavLinks from "./NavLinks";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  const { isSidebarOpen } = useSelector((store) => store.user);
+
+  return (
+    <Wrapper>
+      <div
+        className={
+          isSidebarOpen ? "sidebar-container show-sidebar" : "sidebar-container"
+        }
+      >
+        <div className="content">
+          <header>LOGO</header>
+          <NavLinks />
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Sidebar;
--- src/components/Sidebar/SidebarResponsive.jsx
+++ src/components/Sidebar/SidebarResponsive.jsx
@@ -1,7 +1,37 @@
 import React from "react";
+import { FaTimes } from "react-icons/fa";
+import { useSelector, useDispatch } from "react-redux";
+import { NavLink } from "react-router-dom";
+
+import Wrapper from "../../assets/wrappers/SidebarResponsive";
+import { toggleSidebar } from "../../features/user/userSlice";
+import NavLinks from "./NavLinks";
 
 const SidebarResponsive = () => {
-  return <div>SidebarResponsive</div>;
+  const { isSidebarOpen } = useSelector((store) => store.user);
+  const dispatch = useDispatch();
+
+  const toggle = () => {
+    dispatch(toggleSidebar());
+  };
+
+  return (
+    <Wrapper>
+      <div
+        className={
+          isSidebarOpen ? "sidebar-container show-sidebar" : "sidebar-container"
+        }
+      >
+        <div className="content">
+          <button className="close-btn" onClick={toggle}>
+            <FaTimes />
+          </button>
+          <header>logo</header>
+          <NavLinks toggle={toggle} />
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default SidebarResponsive;
