0c3e0406490d5b38bca57ac12b83d6c58f1b334a
SteTheDiver <stefano.cattaneo86@gmail.com>
2022-07-15 14:38:20 +0200

    Completed SharedLayout

--- src/pages/Dashboard/SharedLayout.jsx
+++ src/pages/Dashboard/SharedLayout.jsx
@@ -1,10 +1,24 @@
 import React from "react";
-
 import { Outlet } from "react-router-dom";
+
 import { Sidebar, SidebarResponsive, Navbar } from "../../components";
+import Wrapper from "../../assets/wrappers/SharedLayout";
 
 const SharedLayout = () => {
-  return <div>SharedLayout</div>;
+  return (
+    <Wrapper>
+      <main className="dashboard">
+        <SidebarResponsive />
+        <Sidebar />
+        <div>
+          <Navbar />
+          <div className="dashboard-page">
+            <Outlet />
+          </div>
+        </div>
+      </main>
+    </Wrapper>
+  );
 };
 
 export default SharedLayout;
