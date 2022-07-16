002b003c182a73c9de5623c68e6c62c3783f4ff4
SteTheDiver <stefano.cattaneo86@gmail.com>
2022-07-16 11:52:24 +0200

    Minor change to center form

--- src/assets/wrappers/SharedLayout.js
+++ src/assets/wrappers/SharedLayout.js
@@ -1,17 +1,15 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
 const Wrapper = styled.section`
   .dashboard {
     display: grid;
     grid-template-columns: 1fr;
   }
-
-  .dashboard-calling {
+  .dashboard-page {
     width: 90vw;
     margin: 0 auto;
     padding: 2rem 0;
   }
-
   @media (min-width: 992px) {
     .dashboard {
       grid-template-columns: auto 1fr;
@@ -20,6 +18,5 @@ const Wrapper = styled.section`
       width: 90%;
     }
   }
-`;
-
-export default Wrapper;
+`
+export default Wrapper
