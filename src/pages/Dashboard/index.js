c6b188a758098351e53fcd0e44d2c5ed629deb39
SteTheDiver <stefano.cattaneo86@gmail.com>
2022-07-17 17:52:40 +0200

    Created component with logic

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -2,5 +2,6 @@ import AllJobs from "./AllJobs";
 import Profile from "./Profile";
 import Statistics from "./Statistics";
 import SharedLayout from "./SharedLayout";
+import AddJob from "./AddJob";
 
-export { Profile, AllJobs, Statistics, SharedLayout };
+export { Profile, AllJobs, AddJob, Statistics, SharedLayout };
