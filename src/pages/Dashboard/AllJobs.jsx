27411ab2de913364aff31e1dc1bba292725661ff
SteTheDiver <stefano.cattaneo86@gmail.com>
2022-07-28 09:57:18 +0200

    Display all jobs

--- src/pages/Dashboard/AllJobs.jsx
+++ src/pages/Dashboard/AllJobs.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+import { JobsContainer, SearchJob } from "../../components";
 
 const AllJobs = () => {
   return (
-    <div>AllJobs</div>
-  )
-}
+    <>
+      <SearchJob />
+      <JobsContainer />
+    </>
+  );
+};
 
-export default AllJobs
+export default AllJobs;
