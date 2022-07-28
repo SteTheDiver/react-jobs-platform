2ebe64662e73a049c5a9c2ae250292ef212955bf
SteTheDiver <stefano.cattaneo86@gmail.com>
2022-07-28 09:57:38 +0200

    Updated with new components

--- src/components/index.js
+++ src/components/index.js
@@ -4,5 +4,22 @@ import Navbar from "./Navbar";
 import Sidebar from "./Sidebar/Sidebar";
 import SidebarResponsive from "./Sidebar/SidebarResponsive";
 import FormSelect from "./FormSelect";
+import Job from "./Job";
+import JobInfo from "./JobInfo";
+import SearchJob from "./SearchJob";
+import JobsContainer from "./JobsContainer";
+import Loader from "./Loader";
 
-export { Logo, FormRow, FormSelect, Navbar, Sidebar, SidebarResponsive };
+export {
+  Logo,
+  FormRow,
+  FormSelect,
+  Navbar,
+  Sidebar,
+  SidebarResponsive,
+  Job,
+  JobInfo,
+  SearchJob,
+  JobsContainer,
+  Loader,
+};
