f0de311720e1c0cf8024907818d3bca4e740d806
SteTheDiver <stefano.cattaneo86@gmail.com>
2022-08-01 08:46:53 +0200

    Created pagination component with logic & styling

---
+++ src/components/PagesButton.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+
+import { HiChevronDoubleLeft, HiChevronDoubleRight } from "react-icons/hi";
+import { useSelector, useDispatch } from "react-redux";
+import { changePage } from "../features/allJobs/allJobsSlice";
+
+import Wrapper from "../assets/wrappers/PagesButton";
+
+const PagesButton = () => {
+  const { numOfPages, page } = useSelector((store) => store.allJobs);
+  const dispatch = useDispatch();
+
+  const pages = Array.from({ length: numOfPages }, (_, index) => {
+    return index + 1;
+  });
+
+  const prevPage = () => {
+    let newPage = page - 1;
+    if (newPage < 1) {
+      newPage = numOfPages;
+    }
+    dispatch(changePage(newPage));
+  };
+  const nextPage = () => {
+    let newPage = page + 1;
+    if (newPage > numOfPages) {
+      newPage = 1;
+    }
+    dispatch(changePage(newPage));
+  };
+
+  return (
+    <Wrapper>
+      <button type="button" className="prev-btn" onClick={prevPage}>
+        prev
+        <HiChevronDoubleLeft />
+      </button>
+      <div className="btn-container">
+        {pages.map((pageNumber) => {
+          return (
+            <button
+              type="button"
+              key={pageNumber}
+              className={pageNumber === page ? "pageBtn active" : "pageBtn"}
+              onClick={() => dispatch(changePage(pageNumber))}
+            >
+              {pageNumber}
+            </button>
+          );
+        })}
+      </div>
+      <button type="button" className="next-btn" onClick={nextPage}>
+        next
+        <HiChevronDoubleRight />
+      </button>
+    </Wrapper>
+  );
+};
+
+export default PagesButton;
