9f3f6c03dcac9861eaca556ceb3f64235e78626d
SteTheDiver <stefano.cattaneo86@gmail.com>
2022-08-02 10:01:59 +0200

    Added condition for when loading

--- src/components/SearchJob.jsx
+++ src/components/SearchJob.jsx
@@ -13,6 +13,7 @@ const SearchJob = () => {
   const dispatch = useDispatch();
 
   const handleSearch = (e) => {
+    if (isLoading) return;
     dispatch(handleChange({ name: e.target.name, value: e.target.value }));
   };
 
